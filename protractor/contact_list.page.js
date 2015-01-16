d0ca41486453dfca81c7b236b48bad5f14d03b42
Erik Dykema <erik@caserails.com>
2015-01-16 12:18:19 -0500

    Tidy things up

---
+++ protractor/contact_list.page.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var Page = function(){
+    this.init = function(){
+        this.bodyHeader = element(by.id('BODY_HEADER'));
+    };
+
+    this.get = function(){
+        browser.get(global.baseUrl + '/#/contacts');
+        this.init();
+    };
+};
+
+module.exports = Page
---
+++ protractor/manage.page.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var Page = function(){
+    this.init = function(){
+        this.bodyHeader = element(by.id('BODY_HEADER'));
+    };
+
+    this.get = function(user_id){
+        browser.get(global.baseUrl+'/#/manage/'+user_id);
+        this.init();
+    };
+};
+
+module.exports = Page
